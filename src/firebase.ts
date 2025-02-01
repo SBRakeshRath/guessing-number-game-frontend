f241bcf2808abbfc7b20d341caa8ff8a4dc3df7d
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2025-02-01 21:35:58 +0530

    updated apigateway and firebase project

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,22 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAxTLoGhTGpSSUa3ByRQ2CWgZxTWs_UCtA",
-    authDomain: "guessing-number-game.firebaseapp.com",
-    projectId: "guessing-number-game",
-    storageBucket: "guessing-number-game.firebasestorage.app",
-    messagingSenderId: "13062997874",
-    appId: "1:13062997874:web:fcfd0830f83d297a13ffeb",
-    measurementId: "G-Z2PEEZSW7L"
-  };
+  apiKey: "AIzaSyCc7Y3zfqWIvBw8w1OXzmVEnp_FzyYKjUo",
+  authDomain: "random-toy-projects.firebaseapp.com",
+  projectId: "random-toy-projects",
+  storageBucket: "random-toy-projects.firebasestorage.app",
+  messagingSenderId: "437863435614",
+  appId: "1:437863435614:web:94b66f38ff5256b4a453cf",
+  measurementId: "G-W7CTENJ6D6",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export { auth }
+export { auth };
